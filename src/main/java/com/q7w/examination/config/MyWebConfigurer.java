5fbf2b7a71e48560c5bf1fc1b45970aa419741ec
Johnson-Zheng <442043039@qq.com>
2020-07-23 13:05:39 +0800

    20200723 新增跨域域名

--- src/main/java/com/q7w/examination/config/MyWebConfigurer.java
+++ src/main/java/com/q7w/examination/config/MyWebConfigurer.java
@@ -14,7 +14,7 @@ public class MyWebConfigurer implements WebMvcConfigurer {
         //所有请求都允许跨域，使用这种配置方法就不能在 interceptor 中再配置 header 了
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080","http://localhost:8082","http://hippo-exam.q7w.cn")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3600);
