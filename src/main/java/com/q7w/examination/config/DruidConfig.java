0ee0ff148e096414123a05708343b3ba37305ab8
gxj <940331234@qq.com>
2020-07-28 21:02:34 +0800

    0728 1、Token时间延长 2、修改拦截路径

--- src/main/java/com/q7w/examination/config/DruidConfig.java
+++ src/main/java/com/q7w/examination/config/DruidConfig.java
@@ -98,4 +98,5 @@ public class DruidConfig {
 
         return datasource;
     }
+
 }
--- src/main/java/com/q7w/examination/config/JwtRealm.java
+++ src/main/java/com/q7w/examination/config/JwtRealm.java
@@ -1,5 +1,7 @@
 package com.q7w.examination.config;
 import cn.hutool.core.lang.Console;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import com.q7w.examination.Service.RedisService;
 import com.q7w.examination.Service.UserService;
 import com.q7w.examination.entity.User;
@@ -20,6 +22,9 @@ import org.apache.shiro.subject.PrincipalCollection;
  **/
 import com.q7w.examination.util.JWTToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 
 /**
@@ -63,7 +68,7 @@ public class JwtRealm extends AuthorizingRealm {
     //        throw new AccountException("JWT token不存在！");
    //     }
 
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, username, getName());
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(username, user, getName());
         Console.log(username+"JWT登录成功");
         return info;
     }
@@ -81,6 +86,45 @@ public class JwtRealm extends AuthorizingRealm {
     //    info.setRoles(roles);
     //    info.setStringPermissions(perms);
         return info;
+    }
+    @Bean
+    public ServletRegistrationBean DruidStatViewServle(){
+
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+
+        //添加初始化参数：initParams
+
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny","192.168.0.114");
+
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+
+        return filterRegistrationBean;
     }
 
 }
